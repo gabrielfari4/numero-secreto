896e42b80d19ad172f1ab2441f59b9ef41c31d62
Gabriel Faria <gabrielfaria88@gmail.com>
2023-10-23 11:31:50 -0300

    generating random number

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const minimumValue = 1;
+const maximumValue = 10;
+const secretNumber = generateRandomNumber();
+
+function generateRandomNumber () {
+    return parseInt(Math.random() * maximumValue + 1);
+}
+
+console.log(secretNumber)
+
+const minimumValueElement = document.getElementById('menor-valor');
+minimumValueElement.innerHTML = minimumValue;
+
+const maximumValueElement = document.getElementById('maior-valor');
+maximumValueElement.innerHTML = maximumValue;
